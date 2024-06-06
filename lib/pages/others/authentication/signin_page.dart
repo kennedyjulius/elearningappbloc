2efe2bdfc7a57023bfd85db5ec74b9792b985e9a
kennedyjulius <kennedymutugi111@gmail.com>
2024-06-06 15:10:36 -0400

    completed

--- lib/pages/others/authentication/signin_page.dart
+++ lib/pages/others/authentication/signin_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:elearning_app_bloc/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
@@ -15,75 +16,88 @@ class SignInPage extends StatefulWidget {
 class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SafeArea(child: Scaffold(
+    return SafeArea(
+      child: Scaffold(
         appBar: buildAppBar(),
         body: SingleChildScrollView(
           child: Column(
             children: [
               buildThirdPartLogin(context),
-              const SizedBox(height: 10,),
-          reusableText("Or use your email account to login"),
-          Container(
-            margin: EdgeInsets.only(
-              top: 66.h,
-            ),
-            padding: EdgeInsets.only(
-              left: 25.w,
-              right: 25.w,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                reusableText("Email"),
-                buildTextField("Enter your Email address", "email" "user"),
-              ],
-            ),
-          ),
+              const SizedBox(height: 10),
+              reusableText("Or use your email account to login"),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 66.h,
+                ),
+                padding: EdgeInsets.only(
+                  left: 25.w,
+                  right: 25.w,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    reusableText("Email"),
+                    SizedBox(height: 5.h,),
+                    buildTextField("Enter your Email address", "email", "user"),
+                    reusableText("Password"),
+                    SizedBox(height: 5.h,),
+                    buildTextField("Enter your Password", "Password", "lock"),
+                    SizedBox(height: 10.h),
+                    forgotPassword(),
+                    SizedBox(height: 10.h,),
+                    buildLogInAndRegButton("Login", ),
+                    buildLogInAndRegButton("Register", ),
+                  ],
+                ),
+              ),
             ],
           ),
-        )
-      )),
+        ),
+      ),
     );
   }
 
   AppBar buildAppBar() {
     return AppBar(
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1.0), 
-          child: Container(
-          color: Colors.grey.withOpacity(0.5),
+      bottom: PreferredSize(
+        preferredSize: Size.fromHeight(1.0),
+        child: Container(
+          color: AppColors.primarySecondaryBackground,
           height: 1.0,
-        )),
-        title: Text("Log in",style: TextStyle(
-          color: Colors.black,
+        ),
+      ),
+      title: Text(
+        "Log in",
+        style: TextStyle(
+          color: AppColors.primaryText,
           fontSize: 16,
-          fontWeight: FontWeight.normal
-        ),),
-      );
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    );
   }
 
-   Widget buildThirdPartLogin (BuildContext context) {
+  Widget buildThirdPartLogin(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(
         top: 40.h,
         bottom: 20.h,
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           reusableIcons("google"),
           reusableIcons("apple"),
           reusableIcons("facebook"),
-          
         ],
       ),
     );
-   }
+  }
 
-   Widget reusableIcons(String iconName){
+  Widget reusableIcons(String iconName) {
     return GestureDetector(
       onTap: () {
-        
+        // Add your onTap logic here
       },
       child: SizedBox(
         width: 40.w,
@@ -91,36 +105,29 @@ class _SignInPageState extends State<SignInPage> {
         child: Image.asset("assets/icons/${iconName}.png"),
       ),
     );
-   }
-      Widget reusableText(String text){
-    return GestureDetector(
-      onTap: () {
-        
-      },
-      child: SizedBox(
-        width: 40.w,
-        height: 40.w,
-        child: Text(text,
+  }
+
+  Widget reusableText(String text) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: Text(
+        text,
         style: TextStyle(
           fontWeight: FontWeight.normal,
-          color: Colors.grey.withOpacity(0.5)
-        ),
+          color: Colors.grey.withOpacity(0.8),
         ),
       ),
     );
-   }
+  }
 
-   Widget buildTextField(String hinttext, String textType, String iconName){
+  Widget buildTextField(String hinttext, String textType, String iconName) {
     return Container(
       width: 325.w,
       height: 50.h,
-      color: Colors.white,
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black),
         color: Colors.grey.withOpacity(0.5),
-        borderRadius: BorderRadius.all(Radius.circular(15.w),
-        
-        )
+        borderRadius: BorderRadius.all(Radius.circular(15.w)),
       ),
       child: Row(
         children: [
@@ -130,50 +137,107 @@ class _SignInPageState extends State<SignInPage> {
             height: 16.w,
             child: Image.asset("assets/icons/$iconName.png"),
           ),
-          Container(
-            width: 270.w,
+          SizedBox(width: 8.w),
+          SizedBox(
             height: 50.h,
+            width: 270.w,
             child: TextField(
               keyboardType: TextInputType.multiline,
               style: TextStyle(
                 fontFamily: "Avenir",
-                fontSize: 12.sp,
+                fontSize: 14.sp,
                 color: Colors.black,
-                fontWeight: FontWeight.normal
+                fontWeight: FontWeight.normal,
               ),
+              autocorrect: false,
+              obscureText: textType=="password"?true:false,
               decoration: InputDecoration(
                 hintText: hinttext,
                 border: OutlineInputBorder(
                   borderSide: BorderSide(
                     color: Colors.transparent,
-                  )
+                  ),
                 ),
                 enabledBorder: OutlineInputBorder(
-                 borderSide: BorderSide(
-                  color: Colors.transparent,
-                 ),
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
+                  ),
                 ),
                 disabledBorder: OutlineInputBorder(
                   borderSide: BorderSide(
                     color: Colors.transparent,
-                  )
+                  ),
                 ),
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(
-                    color: Colors.transparent
-                  )
+                    color: Colors.transparent,
+                  ),
                 ),
                 hintStyle: TextStyle(
-                  color: Colors.transparent
-                )
+                  color: Colors.black.withOpacity(0.5),
+                ),
               ),
-              
             ),
-          )
+          ),
         ],
       ),
     );
-   }
+  }
 
-   
+  Widget forgotPassword(){
+    return Container(
+      margin: EdgeInsets.only(left: 25.w),
+      width: 260.w,
+      height: 44.h,
+      child: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Text("forgot Password",
+        style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w200,
+          decoration: TextDecoration.underline,
+          decorationColor: Colors.blue,
+          fontSize: 12.sp,
+        ),),
+      ),
+    );
+  }
+
+  Widget buildLogInAndRegButton(String buttonName, String buttonType){
+    return GestureDetector(
+      onTap: () {
+        
+      },
+      child: Container(
+        margin: EdgeInsets.only(left: 25.w, right: 25.w, top: 40.h),
+        width: 325.w,
+        height: 50.h,
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: buttonType=="login"?AppColors.primaryElement:AppColors.primaryBackground,
+          ),
+          color: buttonType =="login"?AppColors.primaryElement:AppColors.primaryBackground,
+          borderRadius: BorderRadius.circular(15.w),
+          boxShadow: [
+            BoxShadow(
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: Offset(0, 1),
+              color: AppColors.primaryElement
+            ),
+          ]
+        ),
+        child: Text(
+          buttonName,
+        style: TextStyle(
+          color: buttonType =="login"?AppColors.primaryBackground:AppColors.primaryText,
+          fontSize: 16.sp,
+          fontWeight: FontWeight.normal
+        ),),
+      ),
+    );
+  }
 }
+
