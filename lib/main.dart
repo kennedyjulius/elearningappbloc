910eef881d771a07bae813ea507dca12472d3f90
kennedyjulius <kennedymutugi111@gmail.com>
2024-06-12 13:51:31 -0400

    done on BLOC

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:elearning_app_bloc/bloc_providers.dart';
-import 'package:elearning_app_bloc/pages/others/authentication/register.dart';
-import 'package:elearning_app_bloc/pages/others/authentication/signin_page.dart';
-import 'package:elearning_app_bloc/pages/others/home_page.dart';
+
+import 'package:elearning_app_bloc/routes/pages.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -18,19 +16,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: AppBlocProviders.allBlocProviders,
+      providers: AppPages.allBlocProviders(context), 
       child: ScreenUtilInit(
         builder: (context, child) => MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             useMaterial3: true,
           ),
-          initialRoute: 'signIn',  // Set the initial route if needed
-          routes: {
-            'myHomePage': (context) => MyHomePage(),
-            'signIn': (context) => SignInPage(),
-            'register': (context) => RegisterScreen(),
-          },
+          initialRoute: '/',
+          onGenerateRoute: AppPages.GenerateRouteSettings,
+            // Set the initial route if needed
+          // routes: {
+          //   'myHomePage': (context) => MyHomePage(),
+          //   'signIn': (context) => SignInPage(),
+          //   'register': (context) => RegisterScreen(),
+          // },
         ),
       ),
     );
--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,3 +1,72 @@
+import 'package:elearning_app_bloc/pages/others/authentication/bloc/register_blocs.dart';
+import 'package:elearning_app_bloc/pages/others/authentication/bloc/sign_in_blocs.dart';
+import 'package:elearning_app_bloc/pages/others/authentication/register.dart';
+import 'package:elearning_app_bloc/pages/others/authentication/signin_page.dart';
+import 'package:elearning_app_bloc/pages/others/home/homescreen.dart';
+import 'package:elearning_app_bloc/pages/welcome/bloc/welcome_bloc.dart';
+import 'package:elearning_app_bloc/pages/welcome/welcome.dart';
+import 'package:elearning_app_bloc/routes/names.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AppPages {
+  // Define a list of PageEntity objects representing the routes in the app
+  final List<PageEntity> routes = [
+    PageEntity(
+      route: AppRoutes.INITIAL,
+      page: const Welcome(),
+      bloc: BlocProvider(create: (_) => WelcomeBloc()),
+    ),
+    PageEntity(
+      route: AppRoutes.REGISTER,
+      page: const RegisterScreen(),
+      bloc: BlocProvider(create: (_) => RegisterBlocs()),
+    ),
+    PageEntity(
+      route: AppRoutes.SIGN_IN,
+      page: const SignInPage(),
+      bloc: BlocProvider(create: (_) => SignInBloc()),
+    ),
+    PageEntity(
+      route: AppRoutes.APPLICATION,
+      page: const Homescreen(), 
+      //bloc: null,
+      // Uncomment and provide appropriate BlocProvider if needed
+       bloc: BlocProvider(create: (_) => SomeBloc()),
+    ),
+  ];
+
+  // Static method to retrieve all BlocProviders from the routes
+  static List<BlocProvider> allBlocProviders(BuildContext context) {
+    List<BlocProvider> blocProviders = [];
+    for (var route in AppPages().routes) {
+      blocProviders.add(route.bloc);
+    }
+    return blocProviders;
+  }
+  static MaterialPageRoute GenerateRouteSettings(RouteSettings settings){
+    if (settings.name!==null) {
+    var result = routes().where((element) => element.route==settings.name);
+    if (result.isNotEmpty) {
+    return MaterialPageRoute(builder: (_)=>result.first.page, settings: settings);
+    }
+    }
+    return MaterialPageRoute(
+      builder: (_)=>SignInPage(), 
+      settings: settings
+      );
+  }
+}
+
+// Class representing a page entity with route, page widget, and BlocProvider
 class PageEntity {
-  
+  final String route;
+  final Widget page;
+  final BlocProvider bloc;
+
+  PageEntity({
+    required this.route,
+    required this.page,
+    required this.bloc,
+  });
 }
